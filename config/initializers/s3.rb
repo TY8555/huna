54c2886103bf3e94f6061da17d3bc92b65bce8da
appi123dk <whdidtnr224@naver.com>
2016-09-04 16:27:41 +0000

    image2

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = 'lovehuna'                          # required
   config.fog_public     = true                                       # optional, defaults to true
+  config.storage = :fog
   
 end
 
