9b39edef5d75968ea786502353aa28fc1f1bfc95
appi123dk <whdidtnr224@naver.com>
2016-09-04 15:49:49 +0000

    보완업데이트

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 #   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIFLHLTBKDMACO75Q',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     endpoint:              'http://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
@@ -11,3 +11,4 @@ CarrierWave.configure do |config|
   config.fog_public     = true                                       # optional, defaults to true
   
 end
+
