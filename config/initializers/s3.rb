dff32e2627e8c636a701fe5467796401b17a8c08
appi123dk <whdidtnr224@naver.com>
2016-09-04 17:04:52 +0000

    image c9 upload

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-    region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
-    endpoint:              'http://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
-  }
-  config.fog_directory  = 'lovehuna'                          # required
-  config.fog_public     = true                                       # optional, defaults to true
+  # config.fog_provider = 'fog/aws'                        # required
+  # config.fog_credentials = {
+  #   provider:              'AWS',                        # required
+  #   aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+  #   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+  #   region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
+  #   endpoint:              'http://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
+  # }
+  # config.fog_directory  = 'lovehuna'                          # required
+  # config.fog_public     = true                                       # optional, defaults to true
   
   
 end
