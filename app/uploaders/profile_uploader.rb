7e6bc0b1aae52cb7e9ba78f3dbaa694b62a8f6bd
appi123dk <whdidtnr224@naver.com>
2016-09-04 16:35:12 +0000

    image3

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -12,9 +12,9 @@ class ProfileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/"
-  # end
+  def store_dir
+    "uploads/"
+  end
   
  
   
